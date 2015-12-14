388f5fe3971fb5fe67da6e7e5c3a6d259a503b71
Dexter Miguel <me@dextermiguel.com>
2015-12-14 15:57:59 -0500

    Uploaded day 2

---
+++ day2.exs
@@ -0,0 +1,24 @@
+defmodule AdventOfCode.Day2 do
+
+  def pcalc({l, _}, {w, _}, {h, _}) do
+    min = [l * w, w * h, h * l] |> Enum.min
+    (2 * l * w) + (2 * w * h)  + (2 * h * l) + min
+  end
+
+  def calc([""], total) do
+    total
+  end
+
+  def calc([head|tail], total) do
+    [l, w, h] = head |> String.split("x")
+    calc(tail, total + pcalc(Integer.parse(l), Integer.parse(w), Integer.parse(h)))
+  end
+
+  def calc(input) do
+    String.split(input, "\n") |> calc(0)
+  end
+
+  def solve do
+    "day2input.txt" |> File.read! |> calc
+  end
+end
