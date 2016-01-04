51b233d4b3ee761b52c07f15ae63c59d521d31af
Dexter Miguel <me@dextermiguel.com>
2016-01-04 12:17:59 -0500

    Finished challenge 4

---
+++ lib/day4.ex
@@ -0,0 +1,23 @@
+defmodule Challenges.Day4 do
+  @input "yzbqklnj"
+
+  def solve("00000" <> _, count) do
+    count
+  end
+
+  def solve(_, count) do
+    solve(count + 1)
+  end
+
+  def solve(count) do
+    hash = :md5
+           |> :crypto.hash(@input <> Integer.to_string(count))
+           |> Base.encode16
+
+    solve(hash, count)
+  end
+
+  def solve do
+    solve(0)
+  end
+end
